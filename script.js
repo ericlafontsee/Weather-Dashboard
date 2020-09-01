59408a1841c9c816ca5ba21bd556ecc13b85fa01
Eric LaFontsee <elafontsee@gmail.com>
2020-09-01 09:40:42 -0500

    Added comments to script.js

--- script.js
+++ script.js
@@ -19,19 +19,9 @@ function getLastSearch() {
     return cities[cities.length - 1];
 }
 
-//removes the last item in search results
-function removeCity(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var cities = getCities();
-    var indexToRemove = $(this).attr("id");
-    cities.splice(indexToRemove, 1);
-    localStorage.setItem("cities", JSON.stringify(cities));
-    renderEvents(cities);
-}
-
 //populates the search results list
 function renderEvents(cities) {
+    console.log(cities);
     $("#searchedCities").empty();
     for (var i = cities.length - 1; i >= 0; i--) {
         var index = cities[i];
@@ -65,9 +55,10 @@ $("#searchBtn").on("click", function() {
         alert("Enter a city")
     } else {
         weatherNow(input);
+        storeEvents(input);
+        renderEvents(getCities());
+
     }
-    storeEvents(input);
-    removeCity();
 });
 
 //Queries OWM and sets data to variables that are then set as the text values for corresponding <divs>
@@ -95,6 +86,31 @@ function weatherNow(city) {
             $("#humidity").text(response.list[0].main.humidity + " %");
             $("#windSpeed").text(response.list[0].wind.speed + " MPH");
 
+            //Loops through response.list to filter out results from only 3 oclock each day.
+            for (var i = 0; i < response.list.length; i++) {
+                var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+                var unformatedDate = response.list[i].dt_txt.split(" ")[0];
+                var day = unformatedDate.split("-")[2];
+                var month = unformatedDate.split("-")[1].charAt(1);
+                var year = unformatedDate.split("-")[0];
+                var currentDate = " (" + month + "/" + day + "/" + year + ") ";
+                var iconURL = "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png";
+                var weatherIcon = $("<img>").attr("src", iconURL);
+                if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                    var card = $("<div>").attr("class", "card text-light text-center border border-dark bg-primary");
+                    $("#forecast").append(card);
+                    var cardBody = $("<div>").attr("class", "card-body");
+                    card.append(cardBody);
+                    var cardDate = $("<h4>").attr("class", "card-title text-center p-2").text(currentDate);
+                    cardBody.append(cardDate);
+                    cardBody.append(weatherIcon);
+                    var cardTemp = $("<p>").attr("class", "card-text").text("Temp: " + tempF.toFixed(2) + " \xB0F");
+                    cardBody.append(cardTemp);
+                    var cardHumidity = $("<p>").attr("class", "card-text").text("Humidity: " + response.list[i].main.humidity + " %");
+                    cardBody.append(cardHumidity);
+                }
+            }
+
             //Queries OWM to grab UV index and compares data against conditional to color code the <div> based on severity
             var uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi?appid=7e4c7478cc7ee1e11440bf55a8358ec3&lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lat;
             $.ajax({
@@ -121,44 +137,7 @@ function weatherNow(city) {
 
             });
         });
-    forecast(city);
-}
 
-//Queries OWM and grabs five day forecast
-function forecast(city) {
-    // renderEvents(city);
-    var APIKey = "425f2fa92724cf0af5e0b7fdfb38e26e";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function(response) {
-            for (var i = 0; i < response.list.length; i++) {
-                var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-                var unformatedDate = response.list[i].dt_txt.split(" ")[0];
-                var day = unformatedDate.split("-")[2];
-                var month = unformatedDate.split("-")[1].charAt(1);
-                var year = unformatedDate.split("-")[0];
-                var currentDate = " (" + month + "/" + day + "/" + year + ") ";
-                var iconURL = "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png";
-                var weatherIcon = $("<img>").attr("src", iconURL);
-                if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-                    var card = $("<div>").attr("class", "card text-light text-center border border-dark bg-primary");
-                    $("#forecast").append(card);
-                    var cardBody = $("<div>").attr("class", "card-body");
-                    card.append(cardBody);
-                    var cardDate = $("<h4>").attr("class", "card-title text-center p-2").text(currentDate);
-                    cardBody.append(cardDate);
-                    cardBody.append(weatherIcon);
-                    var cardTemp = $("<p>").attr("class", "card-text").text("Temp: " + tempF.toFixed(2) + " \xB0F");
-                    cardBody.append(cardTemp);
-                    var cardHumidity = $("<p>").attr("class", "card-text").text("Humidity: " + response.list[i].main.humidity + " %");
-                    cardBody.append(cardHumidity);
-                }
-
-            };
-        });
 }
 
 init();
